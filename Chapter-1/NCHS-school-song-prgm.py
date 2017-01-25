85f437f300a076001d2efc813c40eb19d14dbbe9
dwhickox <dwhickox@users.noreply.github.com>
2017-01-25 09:19:15 -0600

    Create the school song program

---
+++ Chapter-1/NCHS-school-song-prgm.py
@@ -0,0 +1,9 @@
+#David Hickox
+# Jan 12 17
+# School Song Program
+# Displays school song 
+
+#actualy prints the song
+print("All hail our school NCHS\nWe'll sing your praises NCHS\n\nWith all our might\nWe will always make a fight")
+#waits for an enter press to exit the program
+input("\nPress Enter to continue") 
