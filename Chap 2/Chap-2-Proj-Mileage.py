4b2a1e3b16a8b46425855cd634e76215bd4e74a5
dwhickox <dwhickox@users.noreply.github.com>
2017-01-25 09:33:18 -0600

    Update Chap-2-Proj-Mileage.py

--- Chap 2/Chap-2-Proj-Mileage.py
+++ Chap 2/Chap-2-Proj-Mileage.py
@@ -16,16 +16,18 @@ import locale
 locale.setlocale( locale.LC_ALL, '' )
 #use locale.currency() for currency formating
 print("Welcome to the Milage Program\n")
+#has the user input all the variables
 
+car = input("What type of car do you drive? \n")
+tank = float(input("What size gas tank does your "+car.title()+" have? \n"))
+mpg = float(input("How many miles per gallon does the "+car.title()+" get?\n"))
+price = float(input("How much does a gallon of gas cost?\n"))
+#does all of the maths
+distance = tank * mpg
+cost = price * tank
+#prints the out come of the maths in the required formats
 
-
-
-
-
-
-
-
-
-
-
+print("A brand new", car.title(), "can go", distance,"miles on one tank of gas!\n")
+print("To fill the tank it will cost you", locale.currency(cost))
+#waits for the user to end the program
 input("\nPress Enter to Exit")
