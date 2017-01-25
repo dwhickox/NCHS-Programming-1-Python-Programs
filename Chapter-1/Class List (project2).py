b65d734d6e30350e78d7141f437f2146ea7a0360
dwhickox <dwhickox@users.noreply.github.com>
2017-01-25 09:16:13 -0600

    Creates Chap 1 Project 2
    Displayes name and classes

---
+++ Chapter-1/Class List (project2).py
@@ -0,0 +1,22 @@
+# David Hickox
+# Jan 12 17
+# HickoxProject2
+# Displayes name and classes
+
+# prints my name and classes in columns and waits for the user to hit enter to end the program
+
+print("David Hickox")
+print()
+print("1st    Band")
+print("2nd    Programming")
+print("3rd    Ap Pysics C")
+print("4th    Lunch")
+print("5th    Ap Lang")
+print("6th    TA for R&D")
+print("7th    Gym")
+print("8th    AP Calc BC")
+print()
+input("Press Enter To Continue")
+
+#this works too
+#input("David Hickox\n\n1st    Band\n2nd    Programming\n3rd    Ap Pysics C\n4th    Lunch\n5th    Ap Lang\n6th    TA for R&D\n7th    Gym\n8th    AP Calc BC\n\nPress Enter to continue")
