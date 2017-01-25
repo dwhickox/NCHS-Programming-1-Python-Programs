d91edd0ce0853011ce749755caee3f81d6296b1a
dwhickox <dwhickox@users.noreply.github.com>
2017-01-25 09:14:13 -0600

    Create the program favorites

---
+++ Chapter-1/Favorites.py
@@ -0,0 +1,10 @@
+# David Hickox
+# Jan 12 17
+# Favorite Program
+# Displayes some of my favorites
+
+# prints my favorites in columns
+print("Favorite Color         Blue")
+print("Favorite Food          Pizza")
+print("Favorite Movie         Gran Torino\n")
+input("Press enter to exit")
