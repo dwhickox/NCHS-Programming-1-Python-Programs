eff727e609e9f55e17905990ab4eccc92d8624fb
dwhickox <dwhickox@users.noreply.github.com>
2017-01-28 11:29:09 -0600

    made the program more concise

--- Chap 2/Chap-2-Proj-Payroll.py
+++ Chap 2/Chap-2-Proj-Payroll.py
@@ -8,12 +8,10 @@
 #	h = height of the array 
 #	array column 1 is pay rate for each person in the above order, and the second column is the hours worked,
 #	the third colum is social security, the fourth is tax, fifth is net pay, sixth is gross pay
-#	sleep, determines how long the computer sleeps inbetween printing lines, turn this to 0 for word vomit
 #	i is just the variable that counts for the for loop 
 
 #imports date time and curency handeling because i hate string formating (this takes the place of #$%sleepf"%) 
 import locale
-import time #time is used in this program to give it an old timey computer feel printing the lines slowly instead of vomiting 4 pay stubs at you rather agressivly
 locale.setlocale( locale.LC_ALL, '' )
 #use locale.currency() for currency formating
 print("Welcome to the Payroll Program")
@@ -22,8 +20,8 @@ w = 6
 h = 4
 #creates array
 array = [[0 for x in range(h)] for y in range(w)] 
+#creates the list of names
 list = ["Justin Lefkowitz", "Jack Taylor", "Rohan Bhargava", "Micheal Clinkert"]
-sleep = .2
 #enters pay rates
 array[0][0] = 8.35
 array[0][1] = 15.5
@@ -37,41 +35,25 @@ for i in range(0, 4):
 	array[2][i] = array[5][i]*.07#ss
 	array[3][i] = array[5][i]*.15#tax
 	array[4][i] = array[5][i]-array[2][i]-array[3][i]#net
+#prints the results in a well formated mannor
 for i in range(0, 4):
 	print("\n\n________________________________________________________________________________")
-	time.sleep(sleep)
 	print("\nName:\t\t\t",list[i])
-	time.sleep(sleep)
 	print("Payrate:\t\t",locale.currency(array[0][i]))
-	time.sleep(sleep)
 	print("Hours Worked:\t\t %.0f"%array[1][i])
-	time.sleep(sleep)
 	print("Gross Pay:\t\t",locale.currency(array[5][i]))
-	time.sleep(sleep)
 	print("\nDeductions:")
-	time.sleep(sleep)
 	print("\tSocial Security:",locale.currency(array[2][i]))
-	time.sleep(sleep)
 	print("\tFederal Tax:\t",locale.currency(array[3][i]))
-	time.sleep(sleep)
 	print("\nNet Pay:\t\t",locale.currency(array[4][i]))
-	time.sleep(sleep)
 	print("________________________________________________________________________________")
-	time.sleep(sleep)
 	print("\nGreat Epstein Company")
-	time.sleep(sleep)
 	print("440 W. Aurora Avenue")
-	time.sleep(sleep)
 	print("Naperville, IL 60540")
-	time.sleep(sleep)
 	print("\nPay to the Order of:",list[i],"\t\t\t\t\t",locale.currency(array[4][i]))
-	time.sleep(sleep)
+	#the format statements right align the text
 	print(format("___________________________", '>80'))
-	time.sleep(sleep)
 	print(format("Mrs. Epstein, Big Boss", '>80'))
 	print("\n________________________________________________________________________________")
-	time.sleep(sleep)
-#prints the results in a well formated mannor
-
 #waits for the user to end the program
 input("\n\nPress Enter to Exit")
