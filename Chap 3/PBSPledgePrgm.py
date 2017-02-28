70a7a75ac81f20b735edc3136565c5d19183e632
dwhickox <dwhickox@users.noreply.github.com>
2017-02-28 09:00:33 -0600

    Update PBSPledgePrgm.py

--- Chap 3/PBSPledgePrgm.py
+++ Chap 3/PBSPledgePrgm.py
@@ -27,12 +27,15 @@ while donname and pledge:
 	numpledge += 1
 	donname = input("What is the donator's name? (Press enter when finished) ")
 	pledge = input("How much did "+donname.title()+" give? (Press enter when finished) ")
+        #keeps the program from trying to add an empty string to the numbers, bad things happen
 	if pledge:
 		pledgetot += float(pledge)
 
 #subtracts one from the pledge number because one extra is added when the while loop runs for the last time
 numpledge -=1
 #prints the end result and does the math for it as well
-print("\nThe total dontation is", locale.currency(pledgetot))
-print("The average donation is",locale.currency(pledgetot/numpledge),"over a total of", numpledge,"donors")
+print("\nTotal # of Donators\t",numpledge)
+print("The total dontation is\t", locale.currency(pledgetot))
+print("The average donation is\t",locale.currency(pledgetot/numpledge))
+print("Thank You for your donation!!")
 input("\nPress Enter to Exit")
