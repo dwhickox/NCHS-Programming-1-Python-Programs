2dc9e37bb31b5c502cb60abb380d520b75464aa4
dwhickox <dwhickox@users.noreply.github.com>
2017-02-28 09:35:54 -0600

    Create Challenge.py

---
+++ Chap 3/Challenge.py
@@ -0,0 +1,23 @@
+#David Hickox
+#Feb 28 16
+#Challenge
+#descirptions are much descriptive
+#variables
+#   count, sentry
+
+
+#creates array if needbe
+#array = [[0 for x in range(h)] for y in range(w)] 
+#imports date time and curency handeling because i hate string formating (this takes the place of #$%.2f"%) 
+import locale
+locale.setlocale( locale.LC_ALL, '' )
+#use locale.currency(var, symbol=True, grouping=True) for currency formating
+print("Welcome to the (enter name here bumbblefack) Program\n")
+
+count = 100000
+while count <=999999:
+    if str(count)[2] == str(count)[5] and str(count)[3] == str(count)[4] and str(count+1)[1] == str(count+1)[5] and str(count+1)[
+    count += 1
+
+
+input("\nPress Enter to Exit")
