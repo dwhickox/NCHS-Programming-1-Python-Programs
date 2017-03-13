718be3b6cbf27fc959f669d9e2f92c4ab1caf68c
dwhickox <dwhickox@users.noreply.github.com>
2017-03-13 09:25:43 -0500

    Update jimmiehans.py

--- Chap 4/jimmiehans.py
+++ Chap 4/jimmiehans.py
@@ -3,9 +3,9 @@
 #Jimmmie Hans, freaky fast physics
 #this will calculate the total of a 15 sandwitch platter
 #variables
-#   array column 0 is prices
-#   array column 1 is choices
-
+#   meat = number of xtra meat
+#   xchz = i
+#   rest of labels are in the program, i got lazy sorry, the labels have not been changed 
 
 #creates array if needbe
 #array = [[0 for x in range(h)] for y in range(w)] 
@@ -14,39 +14,41 @@ import locale
 locale.setlocale( locale.LC_ALL, '' )
 #use locale.currency(var, symbol=True, grouping=True) for currency formating
 print("Welcome to the Jimmie Hans Program\n")
-#i = 12
-array = [[0 for x in range(1)] for y in range(4)] 
-array[[0],[0]] = 21.99 #price for the platter
-array[[0],[1]] = .5 #price of extra cheese per sand
-array[[1],[2]] = 1 #price of xtra meat per sand
-array[[1],[0]] = 0 #cheese 
-array[[1],[1]] = 0 #meat
+i = 21
+base = 21.99 #price for the platter
+chzprice = .50 #price of extra cheese per sand
+meatprice = 1.00 #price of xtra meat per sand
+meat = 0 #number of x meat
+chz = 0 #number of x chz 
+xchz = 0 #cheese 
+xmeat = 0 #meat
 #menu
 print("""
 Pepe...............1
 Big John...........2
 Totally Tuna.......3
 Turkey Tom.........4
-Vito...............5
-Vegi...............6
-Remeber to select 5 sandwitches""")
+Vito...............6
+Vegi...............5
+Remember to select 5 sandwitches""")
 
 #repeat the order proccess 5 times
 for i in range(1,6):
-      array[[1],[2]] = int(input("Sandwitch #"+str(i)+"?")) #stores sand choice
-      array[[1],[3]] = input("Extra Cheese?(Y/N) ")#stores if they want extra cheese on this sand or not
-      if array[[1],[2]] != 6:
-          array[[1],[3]] = input("Extra Meat?(Y/N) ")#stores if they want extra meat
-      else:
-          array[[1],[3]] = "n"
+      sand = int(input("Sandwitch #"+str(i)+"?")) #stores sand choice
+      xchz = input("Extra Cheese?(Y/N) ")
+      #stores if they want extra cheese on this sand or not
+      if sand != 6:
+          xmeat = input("Extra Meat?(Y/N) ")
+          #stores if they want extra meat
+          if xmeat.upper() == "Y":
+              meat = meat + 1
       #addes to the total in 
-      if array[[1],[2]].upper() == "Y":
-          array[[1],[0]] += 1
-      if array[[1],[3]].upper() == "Y":
-          array[[1],[1]] += 1
+      if xchz.upper() == "Y":
+          chz += 1
 
-#total = tax*(baseprice+cheeseextra*chzprice+meatextra*meatprice)
-array[[0],[3]] = 1.0175*(array[[0],[0]]+array[[1],[0]]*array[[0],[1]]+array[[1],[1]]*array[[1],[2]])
+#total == tax*(baseprice+cheeseextra*chzprice+meatextra*meatprice)
+total = 1.0175*(base+chz*chzprice+meat*meatprice)
       
-      
+print ("Your total is", locale.currency(total))      
+
 input("\nPress Enter to Exit")
