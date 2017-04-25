255ee7bde0e7a09a82dad04046d2fa7b56450d78
dwhickox <dwhickox@users.noreply.github.com>
2017-04-25 08:45:34 -0500

    Create GPAData.py

---
+++ Chap 5/GPAData.py
@@ -0,0 +1,17 @@
+# nested sequence of students first name, last name, year, gender, and gpa
+students = [["White", "Snow", 9, "F", 3.56],
+            ["Sprat", "Jack", 12, "M", 2.0],
+            ["Contrary", "Mary", 9, "F", 3.674],
+            ["Dumpty", "Humpty", 11, "M", 2.342],
+            ["Bunny", "Easter", 10, "M", 4.233],
+            ["Wonderland", "Alice", 10, "F", 3.755],
+            ["Bunyon", "Paul", 11, "M", 1.434],
+            ["Penny", "Henny", 9, "F", 2.54],
+            ["Hatter", "Mad", 11, "M", 4.522],
+            ["Munk", "Chip", 10, "M", 3.0],
+            ["Hood", "Red Riding", 10, "F", 3.137],
+            ["Bunny", "Bugs", 11, "M", 2.12],
+            ["Duck", "Daffy", 11, "M", 3.564],
+            ["Ant", "Atom", 12, "M", 3.333],
+            ["Mouse", "Mickey", 10, "M", 3.975],
+            ["Brown", "Charlie", 9, "M", 1.25]]
