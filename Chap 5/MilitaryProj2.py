5cfc5ca658bc20a5c76308e7f096d9f5e2b7529b
dwhickox <dwhickox@users.noreply.github.com>
2017-05-01 09:22:50 -0500

    Update MilitaryProj2.py

--- Chap 5/MilitaryProj2.py
+++ Chap 5/MilitaryProj2.py
@@ -11,10 +11,10 @@
 #   minslice = the min information
 """
 #adds values to MIL_TIME
-MIL_TIME = ("00:45", "15:10", "20:03", "HH,MM", "17:36", "09:48")
-#split to make pylint happy
-MIL_TIME += ("10:11", "XX:09", "11:09", "10:04", "04:20", "05:09", "!8:$4")
+MIL_TIME = ["00:45", "15:10", "20:03", "HH,MM", "17:36", "09:48",
+            "10:11", "XX:09", "11:09", "10:04", "04:20", "05:09", "!8:$4"]
 #initializes the array hight and with variables
+deleted = 0
 H = 13
 W = 3
 #initialises the array, dont really need this but I like it
@@ -24,31 +24,31 @@ print("\nMilitary Time\tStandard Time\tMinutes from Midnight")
 #does all of the logic for the program
 for i in enumerate(MIL_TIME):
     #checks to see if the time is actualy a time and not garbage
-    if MIL_TIME[i[0]][0:2].isdigit() and MIL_TIME[i[0]][3:5].isdigit():
-        #stores military time
-        ARRAY[0][i[0]] = MIL_TIME[i[0]]
-        #stores the hours
-        HRSLICE = MIL_TIME[i[0]][0:2]
-        #store the min
-        MINSLICE = MIL_TIME[i[0]][3:5]
-        #store minutes from midnight
-        ARRAY[2][i[0]] = str(int(HRSLICE)*60+int(MINSLICE))
-        #changes hours to 12 hour time intead of 24
-        if int(HRSLICE) > 12:
-            HRSLICE = str(int(HRSLICE)-12)
-        #changes 00 to 12
-        if int(HRSLICE) == 0:
-            HRSLICE = "12"
-        #gets rid of the extra zero ahead of times of less than 10 hours
-        if HRSLICE[0] == "0":
-            HRSLICE = HRSLICE[1]
-        #store 12 hour time
-        ARRAY[1][i[0]] = HRSLICE+":"+MINSLICE
-        #prints all the information
-        print(ARRAY[0][i[0]], ARRAY[1][i[0]], ARRAY[2][i[0]], sep="\t\t")
-    #This deletes the array entry as per new requirements
-    else:
+    if not(MIL_TIME[i[0]][0:2].isdigit()) or not(MIL_TIME[i[0]][3:5].isdigit()):
         del MIL_TIME[i[0]]
-        print("deleted an entry for bad formatting")
+        deleted += 1
+print("Deleted", deleted,"enteries for bad formating!")
         #MIL_TIME.pop(i[0]) would also work
+for i in enumerate(MIL_TIME):
+    #stores military time
+    ARRAY[0][i[0]] = MIL_TIME[i[0]]
+    #stores the hours
+    HRSLICE = MIL_TIME[i[0]][0:2]
+    #store the min
+    MINSLICE = MIL_TIME[i[0]][3:5]
+    #store minutes from midnight
+    ARRAY[2][i[0]] = str(int(HRSLICE)*60+int(MINSLICE))
+    #changes hours to 12 hour time intead of 24
+    if int(HRSLICE) > 12:
+        HRSLICE = str(int(HRSLICE)-12)
+    #changes 00 to 12
+    if int(HRSLICE) == 0:
+        HRSLICE = "12"
+    #gets rid of the extra zero ahead of times of less than 10 hours
+    if HRSLICE[0] == "0":
+        HRSLICE = HRSLICE[1]
+    #store 12 hour time
+    ARRAY[1][i[0]] = HRSLICE+":"+MINSLICE
+    #prints all the information
+    print(ARRAY[0][i[0]], ARRAY[1][i[0]], ARRAY[2][i[0]], sep="\t\t")
 input("\nPress Enter To Exit")
